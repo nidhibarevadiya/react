05b7b4a72671dc840f6b8446f85a84e828b54c33
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2025-06-30 17:44:32 +0530

    raect

---
+++ Comment  Review/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Comment from './Comment-Reviw/Comment'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Comment/>
+    </>
+  )
+}
+
+export default App
--- Event handler/src/main.jsx
+++ Event handler/src/main.jsx
@@ -1,6 +1,6 @@
 
 
-// main.jsx or index.js
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
