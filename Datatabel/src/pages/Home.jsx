df74dbb5c9e14c5aabd0111085eff46276895189
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2025-07-18 17:48:40 +0530

    react

--- Datatabel/src/pages/Home.jsx
+++ Datatabel/src/pages/Home.jsx
@@ -31,7 +31,6 @@ const Home = () => {
       });
   };
 
-  // ✅ Delete handler
   const handleDelete = (id) => {
     axios
       .delete(`http://localhost:3000/Data/${id}`)
@@ -44,7 +43,7 @@ const Home = () => {
   };
 
   return (
-    <Container sx={{ mt: 6 }}>
+    <Container sx={{ mt: 4 }}>
       <Typography
         variant="h4"
         gutterBottom
@@ -58,22 +57,36 @@ const Home = () => {
         📚 Book Collection
       </Typography>
 
-      <Grid container spacing={4}>
+      <Grid container spacing={4} justifyContent="center">
         {books.map((book) => (
           <Grid item key={book.id} xs={12} sm={6} md={3}>
-            <Card sx={{ p: 2 }}>
+            <Card
+              sx={{
+                height: 410,             
+                width: "270px",           
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                margin:2,
+                padding: 2,
+                boxShadow: 13,
+                borderRadius: 3,
+              }}
+            >
               <CardMedia
                 component="img"
-                height="200"
                 image={book.image}
                 alt={book.name}
                 sx={{
-                  borderRadius: "12px",
+                  height: 160,
+                  width: "50%",
                   objectFit: "cover",
-                  mb: 2,
+                  borderRadius: 2,
                 }}
               />
-              <CardContent>
+
+              <CardContent sx={{ textAlign: "center", flexGrow: 1 }}>
                 <Typography
                   variant="h6"
                   sx={{
@@ -93,41 +106,36 @@ const Home = () => {
                 >
                   ₹{book.price}
                 </Typography>
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  justifyContent="center"
-                  sx={{ mt: 2 }}
-                >
-                  <Button
-                    variant="contained"
-                    size="small"
-                    onClick={() => navigate(`/edit/${book.id}`)}
-                    sx={{
-                      backgroundColor: "#74642f",
-                      "&:hover": {
-                        backgroundColor: "#5f5326",
-                      },
-                    }}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    variant="contained"
-                    size="small"
-                    onClick={() => handleDelete(book.id)}
-                    sx={{
-                      backgroundColor: "#74642f",
-                      "&:hover": {
-                        backgroundColor: "#5f5326",
-                      },
-                    }}
-                  >
-                    Delete
-                  </Button>
-                </Stack>
-
               </CardContent>
+
+              <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => navigate(`/edit/${book.id}`)}
+                  sx={{
+                    backgroundColor: "#74642f",
+                    "&:hover": {
+                      backgroundColor: "#5f5326",
+                    },
+                  }}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => handleDelete(book.id)}
+                  sx={{
+                    backgroundColor: "#74642f",
+                    "&:hover": {
+                      backgroundColor: "#5f5326",
+                    },
+                  }}
+                >
+                  Delete
+                </Button>
+              </Stack>
             </Card>
           </Grid>
         ))}
